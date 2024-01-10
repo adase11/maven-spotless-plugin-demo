f973ad47005212f5eb16f3933791c2e3ee570d46
Austin Dase <adase@fundrise.com>
2024-01-10 09:38:25 -0500

    demonstrate index out of bounds exception

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	./mvnw spotless:apply
---
+++ src/main/java/com.adase.demo/DemoSpotlessIssue.java
@@ -0,0 +1,18 @@
+package com.adase.demo;
+
+public class DemoSpotlessIssue {
+
+	public void demo() {
+		DemoRecord demoRecord = new DemoRecord("x", "y");
+		Object result = switch (demoRecord) {
+			case DemoRecord d when d.x != null -> demoRecord.x();
+			case DemoRecord d when d.y != null -> demoRecord.y();
+			default -> throw new IllegalStateException("Unexpected value: " + demoRecord);
+		};
+		System.out.println(result);
+	}
+
+	record DemoRecord(String x, String y) {
+
+	}
+}
